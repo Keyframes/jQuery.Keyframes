3eb99e0f9575110357e6ed600908f0f0f6d50ba4
Jake Cattrall <krazyjakee@gmail.com>
2014-01-18 20:15:15 -0500

    added better support for transform properties

--- jquery.keyframes.js
+++ jquery.keyframes.js
@@ -2,104 +2,114 @@
     var $createKeyframeStyleTag, animationPlayState, playStateRunning, vendorPrefix;
 
     $createKeyframeStyleTag = function(params) {
-	return $("<style>").attr({
-	    class: "keyframe-style",
-	    id: params.id,
-	    type: "text/css"
-	}).appendTo("head");
+        return $("<style>").attr({
+            class: "keyframe-style",
+            id: params.id,
+            type: "text/css"
+        }).appendTo("head");
     };
 
     $.keyframe = {
-	getVendorPrefix: function() {
-	    var ua;
-	    ua = navigator.userAgent;
-	    if (ua.indexOf("Opera") !== -1) {
-		return "-o-";
-	    } else if (ua.indexOf("MSIE") !== -1) {
-		return "-ms-";
-	    } else if (ua.indexOf("WebKit") !== -1) {
-		return "-webkit-";
-	    } else {
-		return "";
-	    }
-	},
-	isSupported: function() {
-	    var animationSupport, element, pfx;
+        getVendorPrefix: function() {
+            var ua;
+            ua = navigator.userAgent;
+            if (ua.indexOf("Opera") !== -1) {
+                return "-o-";
+            }
+            else if (ua.indexOf("MSIE") !== -1) {
+                return "-ms-";
+            }
+            else if (ua.indexOf("WebKit") !== -1) {
+                return "-webkit-";
+            }
+            else {
+                return "";
+            }
+        },
+        isSupported: function() {
+            var animationSupport, element, pfx;
 
-	    element = $('body').get(0);
-	    animationSupport = false;
+            element = $('body').get(0);
+            animationSupport = false;
 
-	    if (element.style.animationName) {
-		animationSupport = true;
-	    } else {
-		pfx = this.vendorPrefix().slice(1, -1);
-		var property = pfx + "AnimationName";
+            if (element.style.animationName) {
+                animationSupport = true;
+            }
+            else {
+                pfx = this.vendorPrefix().slice(1, - 1);
+                var property = pfx + "AnimationName";
 
-		if (property in element.style) {
-		    animationSupport = true;
-		}
-	    }
+                if (property in element.style) {
+                    animationSupport = true;
+                }
+            }
 
-	    return animationSupport;
-	},
-	generate: function(frameData) {
-	    var $elems, $frameStyle, css, frameName, property;
-	    frameName = frameData.name || "";
-	    css = "@" + (this.getVendorPrefix()) + "keyframes " + frameName + " {";
+            return animationSupport;
+        },
+        generate: function(frameData) {
+            var $elems, $frameStyle, css, frameName, property;
+            frameName = frameData.name || "";
+            css = "@" + (this.getVendorPrefix()) + "keyframes " + frameName + " {";
 
-	    for (key in frameData) {
-		if (key !== "name") {
-		    css += key + " {";
+            for (key in frameData) {
+                if (key !== "name") {
+                    css += key + " {";
 
-		    for (property in frameData[key]) {
-			css += property + ":" + frameData[key][property] + ";";
-		    }
+                    for (property in frameData[key]) {
+                        var propertyNew = property;
+                        if (property == "transform") {
+                            propertyNew = this.getVendorPrefix() + property;
+                        }
+                        css += propertyNew + ":" + frameData[key][property] + ";";
+                    }
 
-		    css += "}";
-		}
-	    }
+                    css += "}";
+                }
+            }
 
-	    css += "}";
+            css += "}";
 
-	    $frameStyle = $("style#" + frameData.name);
+            $frameStyle = $("style#" + frameData.name);
 
-	    if ($frameStyle.length > 0) {
-		$frameStyle.html(css);
+            if ($frameStyle.length > 0) {
+                $frameStyle.html(css);
 
-		$elems = $("*").filter(function() {
-		    return this.style["" + ($.keyframe.vendorPrefix().slice(1, -1)) + "AnimationName"] === frameName;
-		});
+                $elems = $("*").filter(function() {
+                    return this.style["" + ($.keyframe.vendorPrefix().slice(1, - 1)) + "AnimationName"] === frameName;
+                });
 
-		return $elems.each(function() {
-		    var $el, options;
-		    $el = $(this);
-		    options = $el.data("keyframeOptions");
-		    return $el.resetKeyframe(function() {
-			return $el.playKeyframe(options);
-		    });
-		});
-	    } else {
-		return $createKeyframeStyleTag({
-		    id: frameName
-		}).append(css);
-	    }
-	},
-	define: function(frameData) {
-	    var frame, _i, _len, _results;
+                return $elems.each(function() {
+                    var $el, options;
+                    $el = $(this);
+                    options = $el.data("keyframeOptions");
+                    return $el.resetKeyframe(function() {
+                        return $el.playKeyframe(options);
+                    });
+                });
+            }
+            else {
+                return $createKeyframeStyleTag({
+                    id: frameName
+                }).append(css);
+            }
+        },
+        define: function(frameData) {
+            var frame, _i, _len, _results;
 
-	    if (frameData.length) {
-		_results = [];
+            if (frameData.length) {
+                _results = [];
 
-		for (_i = 0, _len = frameData.length; _i < _len; _i++) {
-		    frame = frameData[_i];
-		    _results.push(this.generate(frame));
-		}
+                for (_i = 0, _len = frameData.length; _i < _len; _i++) {
+                    frame = frameData[_i];
+                    _results.push(this.generate(frame));
+                }
 
-		return _results;
-	    } else {
-		return this.generate(frameData);
-	    }
-	}
+                return _results;
+            }
+            else {
+                return this.generate(frameData);
+            }
+        }
     };
 
     vendorPrefix = $.keyframe.getVendorPrefix();
@@ -107,85 +117,85 @@
     playStateRunning = "running";
 
     $.fn.resetKeyframe = function(callback) {
-	var $el;
-	$el = $(this).css(vendorPrefix + animationPlayState, playStateRunning).css(vendorPrefix + "animation", "none");
+        var $el;
+        $el = $(this).css(vendorPrefix + animationPlayState, playStateRunning).css(vendorPrefix + "animation", "none");
 
-	if (callback) {
-	    return setTimeout(callback, 1);
-	}
+        if (callback) {
+            return setTimeout(callback, 1);
+        }
     };
 
     $.fn.pauseKeyframe = function() {
-	var $el;
-	return $el = $(this).css(vendorPrefix + animationPlayState, "paused");
+        var $el;
+        return $el = $(this).css(vendorPrefix + animationPlayState, "paused");
     };
 
     $.fn.resumeKeyframe = function() {
-	return $(this).css(vendorPrefix + animationPlayState, playStateRunning);
+        return $(this).css(vendorPrefix + animationPlayState, playStateRunning);
     };
 
     $.fn.playKeyframe = function(frameOptions, callback) {
-	var animationcss, animationkey, defaultsOptions, delay, duration, frameOptSplit, pfx, repeat, _prefixEvent;
+        var animationcss, animationkey, defaultsOptions, delay, duration, frameOptSplit, pfx, repeat, _prefixEvent;
 
-	if (typeof frameOptions === 'string') {
-	    frameOptSplit = frameOptions.trim().split(' ');
-	    frameOptions = {
-		name: frameOptSplit[0],
-		duration: parseInt(frameOptSplit[1]),
-		timingFunction: frameOptSplit[2],
-		delay: parseInt(frameOptSplit[3]),
-		repeat: frameOptSplit[4],
-		complete: callback
-	    };
-	}
+        if (typeof frameOptions === 'string') {
+            frameOptSplit = frameOptions.trim().split(' ');
+            frameOptions = {
+                name: frameOptSplit[0],
+                duration: parseInt(frameOptSplit[1]),
+                timingFunction: frameOptSplit[2],
+                delay: parseInt(frameOptSplit[3]),
+                repeat: frameOptSplit[4],
+                complete: callback
+            };
+        }
 
-	defaultsOptions = {
-	    duration: 0,
-	    timingFunction: "ease",
-	    delay: 0,
-	    repeat: 1,
-	    direction: "normal",
-	    fillMode: "forwards",
-	    complete: callback
-	};
+        defaultsOptions = {
+            duration: 0,
+            timingFunction: "ease",
+            delay: 0,
+            repeat: 1,
+            direction: "normal",
+            fillMode: "forwards",
+            complete: callback
+        };
 
-	frameOptions = $.extend(defaultsOptions, frameOptions);
+        frameOptions = $.extend(defaultsOptions, frameOptions);
 
-	duration = frameOptions.duration;
-	delay = frameOptions.delay;
-	repeat = frameOptions.repeat;
-	animationcss = "" + frameOptions.name + " " + duration + "ms " + frameOptions.timingFunction + " " + delay + "ms " + repeat + " " + frameOptions.direction + " " + frameOptions.fillMode;
-	callback = frameOptions.complete;
-	animationkey = vendorPrefix + "animation";
-	pfx = ["webkit", "moz", "MS", "o", ""];
+        duration = frameOptions.duration;
+        delay = frameOptions.delay;
+        repeat = frameOptions.repeat;
+        animationcss = "" + frameOptions.name + " " + duration + "ms " + frameOptions.timingFunction + " " + delay + "ms " + repeat + " " + frameOptions.direction + " " + frameOptions.fillMode;
+        callback = frameOptions.complete;
+        animationkey = vendorPrefix + "animation";
+        pfx = ["webkit", "moz", "MS", "o", ""];
 
-	_prefixEvent = function(element, type, callback) {
-	    var evt, p, _results;
-	    p = 0;
-	    _results = [];
-	    while (p < pfx.length) {
-		if (!pfx[p]) {
-		    type = type.toLowerCase();
-		}
-		evt = pfx[p] + type;
-		element.off(evt).on(evt, callback);
-		_results.push(p++);
-	    }
-	    return _results;
-	};
+        _prefixEvent = function(element, type, callback) {
+            var evt, p, _results;
+            p = 0;
+            _results = [];
+            while (p < pfx.length) {
+                if (!pfx[p]) {
+                    type = type.toLowerCase();
+                }
+                evt = pfx[p] + type;
+                element.off(evt).on(evt, callback);
+                _results.push(p++);
+            }
+            return _results;
+        };
 
-	return this.each(function() {
-	    var $el;
-	    $el = $(this).addClass("boostKeyframe").css(vendorPrefix + animationPlayState, playStateRunning).css(animationkey, animationcss).data("keyframeOptions", frameOptions);
-	    if (callback) {
-		_prefixEvent($el, 'AnimationIteration', callback);
-		return _prefixEvent($el, 'AnimationEnd', callback);
-	    }
-	});
+        return this.each(function() {
+            var $el;
+            $el = $(this).addClass("boostKeyframe").css(vendorPrefix + animationPlayState, playStateRunning).css(animationkey, animationcss).data("keyframeOptions", frameOptions);
+            if (callback) {
+                _prefixEvent($el, 'AnimationIteration', callback);
+                return _prefixEvent($el, 'AnimationEnd', callback);
+            }
+        });
     };
 
     $createKeyframeStyleTag({
-	id: "boost-keyframe"
+        id: "boost-keyframe"
     }).append(" .boostKeyframe{" + vendorPrefix + "transform:scale3d(1,1,1);}");
 
 }).call(this);
