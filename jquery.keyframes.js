a35e889e0524eabaa637aa2cd7de26b2cc186eb5
Jake Cattrall <krazyjakee@gmail.com>
2014-04-14 17:27:03 +0100

    Revert "Revert "fix for #20""
    This reverts commit 049dc81d1f325e7e3b5bf5f8569298171bdbe6f7.

--- jquery.keyframes.js
+++ jquery.keyframes.js
@@ -19,6 +19,8 @@
                 return "-ms-";
             } else if (ua.indexOf("WebKit") !== -1) {
                 return "-webkit-";
+            } else if (ua.indexOf("Gecko") !== -1) {
+                return "-moz-";
             } else {
                 return "";
             }
