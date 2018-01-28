5b182acd6bedbf4082cb99a96fe0cdff8daf8ebb
Jake Cattrall <krazyjakee@gmail.com>
2018-01-28 23:39:08 +0100

    move login to keyframes core

--- dist/jquery.keyframes.js
+++ dist/jquery.keyframes.js
@@ -1,129 +1,182 @@
-/* global $ */
-var animationPlayState = 'animation-play-state';
-var playStateRunning = 'running';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var $createKeyframeStyleTag = function $createKeyframeStyleTag(id, css) {
-  return $("<style>".concat(css, "</style>")).attr({
-    class: 'keyframe-style',
-    id: id,
-    type: 'text/css'
-  }).appendTo('head');
-};
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-$.keyframe = {
-  isSupported: function isSupported() {
-    return document.body.style.animationName !== undefined;
-  },
-  generate: function generate(frameData) {
-    var _this = this;
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-    var frameName = frameData.name || '';
-    var css = "@keyframes ".concat(frameName, " {");
+var Keyframes =
+/*#__PURE__*/
+function () {
+  // eslint-disable-line
+  function Keyframes(elem) {
+    _classCallCheck(this, Keyframes);
 
-    for (var key in frameData) {
-      if (key !== 'name' && key !== 'media' && key !== 'complete') {
-        css += "".concat(key, " {");
+    this.elem = elem;
+  }
 
-        for (var property in frameData[key]) {
-          css += "".concat(property, ":").concat(frameData[key][property], ";");
+  _createClass(Keyframes, [{
+    key: "isSupported",
+    value: function isSupported() {
+      return document.body.style.animationName !== undefined;
+    }
+  }, {
+    key: "reset",
+    value: function reset(callback) {
+      this.elem.style.animationPlayState = 'running';
+      this.elem.style.animation = 'none';
+
+      if (callback) {
+        setTimeout(callback, 0);
+      }
+    }
+  }, {
+    key: "pause",
+    value: function pause() {
+      this.elem.style.animationPlayState = 'paused';
+    }
+  }, {
+    key: "resume",
+    value: function resume() {
+      this.elem.style.animationPlayState = 'running';
+    }
+  }, {
+    key: "play",
+    value: function play(frameOptions, callback) {
+      var _this = this;
+
+      var animObjToStr = function animObjToStr(obj) {
+        var newObj = Object.assign({}, {
+          duration: '0s',
+          timingFunction: 'ease',
+          delay: '0s',
+          iterationCount: 1,
+          direction: 'normal',
+          fillMode: 'forwards'
+        }, obj);
+        return [newObj.name, newObj.duration, newObj.timingFunction, newObj.delay, newObj.iterationCount, newObj.direction, newObj.fillMode].join(' ');
+      };
+
+      var animationcss = '';
+
+      if (frameOptions.constructor === Array) {
+        var frameOptionsStrings = [];
+
+        for (var i = 0; i < frameOptions.length; i += 1) {
+          frameOptionsStrings.push(typeof frameOptions[i] === 'string' ? frameOptions[i] : animObjToStr(frameOptions[i]));
         }
 
-        css += '}';
+        animationcss = frameOptionsStrings.join(', ');
+      } else if (typeof frameOptions === 'string') {
+        animationcss = frameOptions;
+      } else {
+        animationcss = animObjToStr(frameOptions);
+      }
+
+      var addEvent = function addEvent(type, eventCallback) {
+        var listenerName = "".concat(type, "Listener");
+
+        _this.elem.removeEventListener(type, _this[listenerName]);
+
+        _this[listenerName] = eventCallback;
+
+        _this.elem.addEventListener(type, _this[listenerName]);
+      };
+
+      this.elem.style.animationPlayState = 'running';
+      this.elem.style.animation = animationcss;
+      this.frameOptions = frameOptions;
+      addEvent('animationiteration', callback || frameOptions.complete);
+      addEvent('animationend', callback || frameOptions.complete);
+    }
+  }], [{
+    key: "createKeyframeTag",
+    value: function createKeyframeTag(id, css) {
+      var elem = document.createElement('style');
+      elem.innerHTML = css;
+      elem.setAttribute('class', 'keyframe-style');
+      elem.setAttribute('id', id);
+      elem.setAttribute('type', 'text/css');
+      document.getElementsByTagName('head')[0].appendChild(elem);
+    }
+  }, {
+    key: "generate",
+    value: function generate(frameData) {
+      var frameName = frameData.name || '';
+      var css = "@keyframes ".concat(frameName, " {");
+
+      for (var key in frameData) {
+        if (key !== 'name' && key !== 'media' && key !== 'complete') {
+          css += "".concat(key, " {");
+
+          for (var property in frameData[key]) {
+            css += "".concat(property, ":").concat(frameData[key][property], ";");
+          }
+
+          css += '}';
+        }
+      }
+
+      if (frameData.media) {
+        css = "@media ".concat(frameData.media, "{").concat(css, "}");
+      }
+
+      var frameStyle = document.getElementById(frameName);
+
+      if (frameStyle) {
+        frameStyle.innerHTML = css;
+      } else {
+        Keyframes.createKeyframeTag(frameName, css);
       }
     }
-
-    if (frameData.media) {
-      css = "@media ".concat(frameData.media, "{").concat(css, "}");
-    }
-
-    var $frameStyle = $("style#".concat(frameData.name));
-
-    if ($frameStyle.length > 0) {
-      $frameStyle.html(css);
-      var $elems = $('*').filter(function () {
-        return _this.style.animationName === frameName;
-      });
-      $elems.each(function () {
-        var $el = $(_this);
-        var options = $el.data('keyframeOptions');
-        $el.resetKeyframe(function () {
-          $el.playKeyframe(options);
-        });
-      });
-    } else {
-      $createKeyframeStyleTag(frameName, css);
-    }
-  },
-  define: function define(frameData) {
-    if (frameData.length) {
-      for (var i = 0; i < frameData.length; i += 1) {
-        var frame = frameData[i];
-        this.generate(frame);
+  }, {
+    key: "define",
+    value: function define(frameData) {
+      if (frameData.length) {
+        for (var i = 0; i < frameData.length; i += 1) {
+          this.generate(frameData[i]);
+        }
+      } else {
+        this.generate(frameData);
       }
-    } else {
-      this.generate(frameData);
     }
-  }
-};
+  }]);
 
-$.fn.resetKeyframe = function (callback) {
-  $(this).css(animationPlayState, playStateRunning).css('animation', 'none');
-
-  if (callback) {
-    setTimeout(callback, 1);
-  }
-};
-
-$.fn.pauseKeyframe = function () {
-  $(this).css(animationPlayState, 'paused');
-};
-
-$.fn.resumeKeyframe = function () {
-  $(this).css(animationPlayState, playStateRunning);
-};
-
-$.fn.playKeyframe = function (frameOptions, callback) {
-  var animObjToStr = function animObjToStr(obj) {
-    var newObj = $.extend({
-      duration: '0s',
-      timingFunction: 'ease',
-      delay: '0s',
-      iterationCount: 1,
-      direction: 'normal',
-      fillMode: 'forwards'
-    }, obj);
-    return [newObj.name, newObj.duration, newObj.timingFunction, newObj.delay, newObj.iterationCount, newObj.direction, newObj.fillMode].join(' ');
+  return Keyframes;
+}();(function () {
+  var doForEach = function doForEach($el, cb) {
+    $el.each(function (index, elem) {
+      cb(new Keyframes(elem));
+    });
   };
 
-  var animationcss = '';
-
-  if ($.isArray(frameOptions)) {
-    var frameOptionsStrings = [];
-
-    for (var i = 0; i < frameOptions.length; i += 1) {
-      frameOptionsStrings.push(typeof frameOptions[i] === 'string' ? frameOptions[i] : animObjToStr(frameOptions[i]));
-    }
-
-    animationcss = frameOptionsStrings.join(', ');
-  } else if (typeof frameOptions === 'string') {
-    animationcss = frameOptions;
-  } else {
-    animationcss = animObjToStr(frameOptions);
-  }
-
-  var addEvent = function addEvent(element, type, eventCallback) {
-    element.off(type).on(type.toLowerCase(), eventCallback || frameOptions.complete);
+  $.keyframe = {
+    isSupported: Keyframes.isSupported,
+    generate: Keyframes.generate,
+    define: Keyframes.define
   };
 
-  this.each(function () {
-    var $el = $(this).addClass('boostKeyframe').css({
-      animationPlayState: playStateRunning,
-      animation: animationcss
-    }).data('keyframeOptions', frameOptions);
-    addEvent($el, 'AnimationIteration', callback);
-    addEvent($el, 'AnimationEnd', callback);
-  });
-  return this;
-};
+  $.fn.resetKeyframe = function (cb) {
+    doForEach($(this), function (kf) {
+      return kf.reset(cb);
+    });
+  };
 
-$createKeyframeStyleTag('boost-keyframe', ' .boostKeyframe{transform:scale3d(1,1,1);}');
+  $.fn.pauseKeyframe = function () {
+    doForEach($(this), function (kf) {
+      return kf.pause();
+    });
+  };
+
+  $.fn.resumeKeyframe = function () {
+    doForEach($(this), function (kf) {
+      return kf.resume();
+    });
+  };
+
+  $.fn.playKeyframe = function (frameOptions, callback) {
+    doForEach($(this), function (kf) {
+      return kf.play(frameOptions, callback);
+    });
+  };
+})();
+
--- src/jquery.keyframes.js
+++ src/jquery.keyframes.js
@@ -1,140 +1,30 @@
-/* global $ */
-
+/* global $, Keyframes */
 (() => {
-    const animationPlayState = 'animation-play-state';
-    const playStateRunning = 'running';
-
-    const $createKeyframeStyleTag = (id, css) => $(`<style>${css}</style>`).attr({
-        class: 'keyframe-style',
-        id,
-        type: 'text/css',
-    }).appendTo('head');
-
-    $.keyframe = {
-        isSupported() {
-            return document.body.style.animationName !== undefined;
-        },
-        generate(frameData) {
-            const frameName = frameData.name || '';
-            let css = `@keyframes ${frameName} {`;
-            for (const key in frameData) {
-                if (key !== 'name' && key !== 'media' && key !== 'complete') {
-                    css += `${key} {`;
-
-                    for (const property in frameData[key]) {
-                        css += `${property}:${frameData[key][property]};`;
-                    }
-
-                    css += '}';
-                }
-            }
-
-            if (frameData.media) {
-                css = `@media ${frameData.media}{${css}}`;
-            }
-
-            const $frameStyle = $(`style#${frameData.name}`);
-
-            if ($frameStyle.length > 0) {
-                $frameStyle.html(css);
-
-                const $elems = $('*').filter(() => this.style.animationName === frameName);
-
-                $elems.each(() => {
-                    const $el = $(this);
-                    const options = $el.data('keyframeOptions');
-                    $el.resetKeyframe(() => {
-                        $el.playKeyframe(options);
-                    });
-                });
-            } else {
-                $createKeyframeStyleTag(frameName, css);
-            }
-        },
-        define(frameData) {
-            if (frameData.length) {
-                for (let i = 0; i < frameData.length; i += 1) {
-                    const frame = frameData[i];
-                    this.generate(frame);
-                }
-            } else {
-                this.generate(frameData);
-            }
-        },
+    const doForEach = ($el, cb) => {
+        $el.each((index, elem) => {
+            cb(new Keyframes(elem));
+        });
     };
 
-    $.fn.resetKeyframe = function (callback) {
-        $(this).css(animationPlayState, playStateRunning).css('animation', 'none');
+    $.keyframe = {
+        isSupported: Keyframes.isSupported,
+        generate: Keyframes.generate,
+        define: Keyframes.define,
+    };
 
-        if (callback) {
-            setTimeout(callback, 1);
-        }
+    $.fn.resetKeyframe = function (cb) {
+        doForEach($(this), kf => kf.reset(cb));
     };
 
     $.fn.pauseKeyframe = function () {
-        $(this).css(animationPlayState, 'paused');
+        doForEach($(this), kf => kf.pause());
     };
 
     $.fn.resumeKeyframe = function () {
-        $(this).css(animationPlayState, playStateRunning);
+        doForEach($(this), kf => kf.resume());
     };
 
     $.fn.playKeyframe = function (frameOptions, callback) {
-        const animObjToStr = function (obj) {
-            const newObj = $.extend({
-                duration: '0s',
-                timingFunction: 'ease',
-                delay: '0s',
-                iterationCount: 1,
-                direction: 'normal',
-                fillMode: 'forwards',
-            }, obj);
-
-            return [
-                newObj.name,
-                newObj.duration,
-                newObj.timingFunction,
-                newObj.delay,
-                newObj.iterationCount,
-                newObj.direction,
-                newObj.fillMode,
-            ].join(' ');
-        };
-
-        let animationcss = '';
-
-        if ($.isArray(frameOptions)) {
-            const frameOptionsStrings = [];
-            for (let i = 0; i < frameOptions.length; i += 1) {
-                frameOptionsStrings.push(typeof frameOptions[i] === 'string' ?
-                    frameOptions[i] :
-                    animObjToStr(frameOptions[i]));
-            }
-            animationcss = frameOptionsStrings.join(', ');
-        } else if (typeof frameOptions === 'string') {
-            animationcss = frameOptions;
-        } else {
-            animationcss = animObjToStr(frameOptions);
-        }
-
-        const addEvent = (element, type, eventCallback) => {
-            element.off(type).on(type.toLowerCase(), eventCallback || frameOptions.complete);
-        };
-
-        this.each(function () {
-            const $el = $(this).addClass('boostKeyframe')
-                .css({
-                    animationPlayState: playStateRunning,
-                    animation: animationcss,
-                })
-                .data('keyframeOptions', frameOptions);
-
-            addEvent($el, 'AnimationIteration', callback);
-            addEvent($el, 'AnimationEnd', callback);
-        });
-
-        return this;
+        doForEach($(this), kf => kf.play(frameOptions, callback));
     };
-
-    $createKeyframeStyleTag('boost-keyframe', ' .boostKeyframe{transform:scale3d(1,1,1);}');
 })();
