28c191bda0fd84a0b787d7f13dfe4258a4c4357f
Jacob Cattrall <krazyjakee@gmail.com>
2019-02-15 00:15:46 +0100

    update keyframes

--- dist/jquery.keyframes.js
+++ dist/jquery.keyframes.js
@@ -65,17 +65,47 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+    return target;
+  };
 
-require('es6-object-assign/auto');
+  return _extends.apply(this, arguments);
+}
 
-var Keyframes =
-/*#__PURE__*/
-function () {
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var Keyframes = function () {
   function Keyframes(elem) {
     _classCallCheck(this, Keyframes);
 
@@ -149,7 +179,7 @@ function () {
     key: "playCSS",
     value: function playCSS(frameOptions) {
       var animObjToStr = function animObjToStr(obj) {
-        var newObj = Object.assign({}, {
+        var newObj = _extends({}, {
           duration: '0s',
           timingFunction: 'ease',
           delay: '0s',
@@ -157,6 +187,7 @@ function () {
           direction: 'normal',
           fillMode: 'forwards'
         }, obj);
+
         return [newObj.name, newObj.duration, newObj.timingFunction, newObj.delay, newObj.iterationCount, newObj.direction, newObj.fillMode].join(' ');
       };
 
@@ -267,62 +298,5 @@ if (typeof document !== 'undefined') {
 
 var _default = Keyframes;
 exports.default = _default;
-
-},{"es6-object-assign/auto":3}],3:[function(require,module,exports){
-'use strict';
-
-require('./index').polyfill();
-
-},{"./index":4}],4:[function(require,module,exports){
-/**
- * Code refactored from Mozilla Developer Network:
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
- */
-'use strict';
-
-function assign(target, firstSource) {
-  if (target === undefined || target === null) {
-    throw new TypeError('Cannot convert first argument to object');
-  }
-
-  var to = Object(target);
-
-  for (var i = 1; i < arguments.length; i++) {
-    var nextSource = arguments[i];
-
-    if (nextSource === undefined || nextSource === null) {
-      continue;
-    }
-
-    var keysArray = Object.keys(Object(nextSource));
-
-    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-      var nextKey = keysArray[nextIndex];
-      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-
-      if (desc !== undefined && desc.enumerable) {
-        to[nextKey] = nextSource[nextKey];
-      }
-    }
-  }
-
-  return to;
-}
-
-function polyfill() {
-  if (!Object.assign) {
-    Object.defineProperty(Object, 'assign', {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: assign
-    });
-  }
-}
-
-module.exports = {
-  assign: assign,
-  polyfill: polyfill
-};
 
 },{}]},{},[1]);
